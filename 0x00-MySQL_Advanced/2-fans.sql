9743365c98806a40ad5aca0a9b0b220ee678287f
danimohli <danimohlife@gmail.com>
2024-10-17 12:56:00 +0100

    edit

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -3,7 +3,5 @@
 -- The result will be ordered by the total number of fans in descending order.
 
 -- Select country origins and the total number of fans
-SELECT origin, SUM(fans) AS total_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY total_fans DESC;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
