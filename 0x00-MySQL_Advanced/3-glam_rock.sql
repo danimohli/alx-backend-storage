cb40159828dbc3279d686abc58d87454e18eaa31
danimohli <danimohlife@gmail.com>
2024-10-17 13:01:39 +0100

    rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,11 +4,7 @@
 -- columns, with a default year of 2022 if the band hasn't split.
 
 -- Select bands with Glam rock as their main style and calculate their lifespan
-SELECT band_name, 
-       CASE 
-           WHEN split IS NOT NULL THEN split - formed
-           ELSE 2022 - formed
-       END AS lifespan
+SELECT band_name,
 FROM metal_bands
 WHERE style LIKE '%Glam%'
 ORDER BY lifespan DESC;
