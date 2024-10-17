51ab5992c566b81bf5c7bf33bedee0e94af3cf24
danimohli <danimohlife@gmail.com>
2024-10-17 21:31:33 +0100

    safediv

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,25 +1,14 @@
 -- Task: Create a function SafeDiv that divides the first number by the second and returns 0 if the second number is 0.
 
+DELIMITER //
 -- Drop the function if it already exists
 DROP FUNCTION IF EXISTS SafeDiv;
 
 -- Create the SafeDiv function
-DELIMITER //
-
-CREATE FUNCTION SafeDiv(
-    a INT,
-    b INT
-)
-RETURNS DECIMAL(10, 2)
-DETERMINISTIC
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    -- Check if the denominator is zero, return 0 in that case
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        -- Otherwise, perform the division
-        RETURN a / b;
-    END IF;
+	RETURN (IF (b = 0, 0, a / b));
 END //
 
 DELIMITER ;
