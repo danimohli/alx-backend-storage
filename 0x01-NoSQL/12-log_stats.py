149682760a6d85e6a3bc438c7c8c8c0760825379
danimohli <danimohlife@gmail.com>
2024-10-22 17:30:40 +0100

    log stat

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,30 +2,34 @@
 """
 Script to provide stats about Nginx logs stored in MongoDB.
 """
-from pymongo import MongoClient
 
+from pymongo import MongoClient
 
 def nginx_stats():
     """
     Display statistics about Nginx logs stored in MongoDB.
     """
+    # Connect to MongoDB
     client = MongoClient()
-    db = client.logs
-    colln = db.nginx
+    db = client.logs  # Access the logs database
+    collection = db.nginx  # Access the nginx collection
 
-    total_logs = colln.count_documents({})
+    # Count total logs
+    total_logs = collection.count_documents({})
 
+    # Count methods
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    metd_c = {method: colln.count_documents({"method": method}) for method in methods}
+    method_counts = {method: collection.count_documents({"method": method}) for method in methods}
 
-    get_status_count = colln.count_documents({"method": "GET", "path": "/status"})
+    # Count logs with method GET and path /status
+    get_status_count = collection.count_documents({"method": "GET", "path": "/status"})
 
+    # Display the statistics
     print(f"{total_logs} logs")
     print("Methods:")
     for method in methods:
         print(f"\t{method_counts[method]}")
     print(f"{get_status_count} logs where method=GET and path=/status")
-
 
 if __name__ == "__main__":
     nginx_stats()
