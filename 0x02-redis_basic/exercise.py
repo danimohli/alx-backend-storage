3928bc26d7e9956c1fa1789e58bc0cfe3ec943fc
danimohli <danimohlife@gmail.com>
2024-10-23 16:00:08 +0100

    read & recover

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Python Exercise for redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -21,7 +21,7 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the given data in Redis with a random key and return the key.
-
+        
         Args:
             data (Union[str, bytes, int, float]): The data to store.
 
@@ -31,3 +31,51 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, bytes, int, float]]:
+        """
+        Retrieve the value from Redis and apply an
+        optional callable to convert it to the desired type.
+
+        Args:
+            key (str): The Redis key.
+            fn (Optional[Callable]): A callable function to
+            apply on the retrieved data for conversion.
+
+        Returns:
+            Optional[Union[str, bytes, int, float]]: The value from Redis,
+            converted by fn if provided, or None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string value from Redis by automatically converting
+        bytes to a string.
+
+        Args:
+            key (str): The Redis key.
+
+        Returns:
+            Optional[str]: The value as a string, or None if the key does not exist.
+        """
+        return self.get(key, lambda data: data.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer value from Redis by automatically
+        converting bytes to an integer.
+
+        Args:
+            key (str): The Redis key.
+
+        Returns:
+            Optional[int]: The value as an integer, or
+            None if the key does not exist.
+        """
+        return self.get(key, lambda data: int(data))
